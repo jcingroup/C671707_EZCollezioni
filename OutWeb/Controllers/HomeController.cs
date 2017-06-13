0c01716df66d431739e6366eb2a787468f79b46d
TinWu <yyuuix@gmail.com>
2017-06-13 12:07:54 +0800

    proj up

---
+++ OutWeb/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System.Web.Mvc;
+
+namespace Work.WebProj.Controllers
+{
+    public class HomeController : Controller
+    {
+        public HomeController()
+        {
+            ViewBag.IsFirstPage = false;
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult AboutUs()
+        {
+            return View();
+        }
+
+        public ActionResult ContactUs()
+        {
+            return View();
+        }
+    }
+}
